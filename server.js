5b755876e7c01a79d6cf3a4c6d0b10f28c81fe9a
Bini, Matteo <binim@fermimn.edu.it>
2023-08-29 19:12:42 +0200

    Little cleaning

--- server.js
+++ server.js
@@ -10,18 +10,8 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-
 const validUsername = 'kev';
 const validPassword = 'kev';
-
-// Middleware to check if user is authenticated
-function isAuthenticated(req, res, next) {
-    if (req.session.isAuthenticated) {
-        return next();
-    } else {
-        res.render('/login.ejs');
-    }
-}
 
 app.set("view engine", "ejs");
 app.set('case sensitive routing', true);
@@ -37,12 +27,20 @@ app.use(
         saveUninitialized: false
     })
 );
-
 app.use(express.static(path.join(__dirname, 'static')));
+
+// Middleware to check if user is authenticated
+function isAuthenticated(req, res, next) {
+    if (req.session.isAuthenticated) {
+        return next();
+    } else {
+        res.render('/login.ejs');
+    }
+}
 
 app.post("/login", (req, res) => {
     const { username, password } = req.body;
-    
+
     if (username === validUsername && password === validPassword) {
         req.session.isAuthenticated = true;
         res.redirect('/');
