2381af9d3b3aad858a30a61b05ed6638b53024e8
Bini, Matteo <binim@fermimn.edu.it>
2023-08-29 18:58:53 +0200

    Basic design. Login working (with plain creds)

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require(`express`);
+
+const multer = require(`multer`);
+const bodyParser = require('body-parser');
+const session = require("express-session");
+const path = require(`path`);
+
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+
+const validUsername = 'kev';
+const validPassword = 'kev';
+
+// Middleware to check if user is authenticated
+function isAuthenticated(req, res, next) {
+    if (req.session.isAuthenticated) {
+        return next();
+    } else {
+        res.render('/login.ejs');
+    }
+}
+
+app.set("view engine", "ejs");
+app.set('case sensitive routing', true);
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(
+    session({
+        secret: "ugo",
+        resave: false,
+        saveUninitialized: false
+    })
+);
+
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.post("/login", (req, res) => {
+    const { username, password } = req.body;
+    
+    if (username === validUsername && password === validPassword) {
+        req.session.isAuthenticated = true;
+        res.redirect('/');
+    } else {
+        res.redirect('/');
+    }
+});
+
+app.get("/", (req, res) => {
+    if (req.session.isAuthenticated)
+        res.render("home.ejs");
+    else
+        res.render("login.ejs");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
+
+
+
